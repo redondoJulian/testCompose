68f6e69d3cebfeda1de33db0a1651c07e758f57b
redondoJulian <juliantw3012@gmail.com>
2025-06-04 11:43:28 -0300

    duplicated

--- app/src/main/java/com/example/pruebacompose/MainActivity.kt
+++ app/src/main/java/com/example/pruebacompose/MainActivity.kt
@@ -6,13 +6,14 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 //import com.example.pruebacompose.layout.BoxColumnText
 import com.example.pruebacompose.layout.ScaffoldLayout
+import com.example.pruebacompose.layout.ScaffoldLayout1
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            ScaffoldLayout()
+            ScaffoldLayout1()
         }
     }
 }
--- app/src/main/java/com/example/pruebacompose/layout/ScaffoldLayout.kt
+++ app/src/main/java/com/example/pruebacompose/layout/ScaffoldLayout.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -25,30 +26,84 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.text.font.FontWeight
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ScaffoldLayout() {
-    var contador by remember { mutableStateOf(0)}
+fun ScaffoldLayout1(){
     Scaffold(
         topBar = {
-            //TopAppBar(title = "App bar")
+            TopAppBar(
+                title = {Text(text = "Prueba",
+                    fontSize = 30.sp,
+                    fontWeight = FontWeight.Bold)},
+                colors = TopAppBarDefaults.topAppBarColors(containerColor = Color.LightGray))
         },
         floatingActionButton = {
-        FloatingActionButton(
-            onClick = {contador++},
-            shape = CircleShape,
-            containerColor = Color.White,
-            contentColor = Color.DarkGray) {
-            Text(text = "+", fontSize = 20.sp)
+            FloatingActionButton(
+                onClick = {},
+                contentColor = Color.Gray,
+                containerColor = Color.White,
+                shape = CircleShape)
+            {Text(text = "+", fontSize = 24.sp, fontWeight = FontWeight.Bold)}
         }
-    }){
-        innerPadding ->
-        Box(modifier = Modifier.padding(innerPadding)
-            .background(color = Color.LightGray)
-            .fillMaxSize()) {
-            Text(text = "Contador número $contador", modifier = Modifier.padding(16.dp),
-                color = Color.White,
-                fontSize = 23.sp)
+    ) { innerPadding ->
+        Box(modifier = Modifier
+            .padding(innerPadding)
+            .fillMaxSize()
+            .background(color = Color.DarkGray)){
+            Column(modifier = Modifier.padding(vertical = 15.dp)) {
+                Box(
+                    modifier = Modifier
+                        //.background(color = Color.Red)
+                        .fillMaxWidth()
+                        .padding(vertical = 5.dp, horizontal = 20.dp)
+                ) {
+                    Row(
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        Text(
+                            text = "Julián",
+                            fontSize = 24.sp,
+                            color = Color.White,
+                            fontWeight = FontWeight.Bold
+                        )
+                        Text(
+                            text = "1160476411",
+                            fontSize = 18.sp,
+                            color = Color.LightGray,
+                            fontWeight = FontWeight.Light
+                        )
+                    }
+                }
+                Box(
+                    modifier = Modifier
+                        //.background(color = Color.Red)
+                        .fillMaxWidth()
+                        .padding(vertical = 5.dp, horizontal = 20.dp)
+                ) {
+                    Row(
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        Text(
+                            text = "Jesica",
+                            fontSize = 24.sp,
+                            color = Color.White,
+                            fontWeight = FontWeight.Bold
+                        )
+                        Text(
+                            text = "1170866905",
+                            fontSize = 18.sp,
+                            color = Color.LightGray,
+                            fontWeight = FontWeight.Light
+                        )
+                    }
+                }
+            }
         }
     }
 }
